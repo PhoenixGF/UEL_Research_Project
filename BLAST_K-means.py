ce581f8d4f63fe5839a9fe92ab66c9656ca342e5
PhoenixGF <80475110+PhoenixGF@users.noreply.github.com>
2021-03-11 16:31:59 +0000

    Update BLAST_K-means.py

--- BLAST_K-means.py
+++ BLAST_K-means.py
@@ -3,19 +3,7 @@
 # how to choose clusters? start low, keep increasing k as until standard error stops decreasing
 
 from numpy import array
-# from numpy import random
-# ##  Create fake income/age clusters for N people in k clusters
-# def createClusteredData(N, k):
-#     random.seed(10)
-#     pointsPerCluster = float(N)/k
-#     X = []
-#     for i in range (k):
-#         incomeCentroid = random.uniform(20000.0, 200000.0)
-#         ageCentroid = random.uniform(20.0, 70.0)
-#         for j in range(int(pointsPerCluster)):
-#             X.append([random.normal(incomeCentroid, 10000.0), random.normal(ageCentroid, 2.0)])
-#     X = array(X)
-#     return X
+
 idlist = []
 numlist = []
 ncount = 0
@@ -41,11 +29,9 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
 from numpy import float
 
-# data = createClusteredData(100, 5)
-
 model = KMeans(n_clusters=10)  # you wouldn't know this value in reality
 
-# Scaling to compare 20-70 to 20000-200000, important to make data comparable
+# Scaling to make data comparable
 model = model.fit(scale(data))
 
 ##  We can look at the clusters each data point was assigned to
